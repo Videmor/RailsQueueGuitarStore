893bc12753e7dce6e6d2b1652a4b7fd26aab97e9
Caike Souza <carloshrsouza@gmail.com>
2012-09-11 15:09:50 -0400

    created Rails edge project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+GuitarStore::Application.config.secret_token = 'f7edde03c02174a12f8d83eb5e5dc1ed2056ad532e4083acfc3419d35618dd8bcf69660bed746d67d9c09a73e59c51ff56f6e50ca45a5da669a7144a87a72f08'
